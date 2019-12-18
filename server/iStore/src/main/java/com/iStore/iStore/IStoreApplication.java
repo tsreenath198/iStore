cf00a5034f84067311c3c5ba0ba521acaba4d592
gangasaiuskcorp <gangasai.uskcorp@gmail.com>
2019-12-18 11:36:08 +0530

    Server first cut

---
+++ server/iStore/src/main/java/com/iStore/iStore/IStoreApplication.java
@@ -0,0 +1,13 @@
+package com.iStore.iStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IStoreApplication.class, args);
+	}
+
+}
---
+++ server/iStore/src/test/java/com/iStore/iStore/IStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iStore.iStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
