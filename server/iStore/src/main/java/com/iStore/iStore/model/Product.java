f2785ae588fdcf5794ec5e16851cc74875baf938
gangasaiuskcorp <gangasai.uskcorp@gmail.com>
2019-12-18 17:26:22 +0530

    First cut with all rest services

--- server/iStore/src/main/java/com/iStore/iStore/model/Product.java
+++ server/iStore/src/main/java/com/iStore/iStore/model/Product.java
@@ -1,10 +1,18 @@
 package com.iStore.iStore.model;
 
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -15,14 +23,23 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
-public class Product extends BaseModel {
+public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
 	@Column
 	@NotNull
-	@NotEmpty
 	private String name;
 	@Column
 	@NotNull
-	@NotEmpty
 	private Float price;
+	@Column
+	@CreationTimestamp
+	private Date createdDate;
+	@Column
+	@UpdateTimestamp
+	private Date updatedDate;
+	@Column
+	private String description;
 
 }
