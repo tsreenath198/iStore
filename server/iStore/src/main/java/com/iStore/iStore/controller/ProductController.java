27b9d413a9151c2d3debda42644753b7b7ec6037
gangasaiuskcorp <gangasai.uskcorp@gmail.com>
2019-12-19 09:26:02 +0530

    Added method actions

--- server/iStore/src/main/java/com/iStore/iStore/controller/ProductController.java
+++ server/iStore/src/main/java/com/iStore/iStore/controller/ProductController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,17 +35,17 @@ public class ProductController {
 		return new ResponseEntity<Product>(productService.update(product), HttpStatus.OK);
 	}
 
-	@PostMapping(ISTOREConstants.DELETE)
+	@GetMapping(ISTOREConstants.DELETE)
 	public ResponseEntity<GenericResponse> delete(@RequestParam Integer id) {
 		return new ResponseEntity<GenericResponse>(productService.delete(id), HttpStatus.OK);
 	}
 
-	@PostMapping(ISTOREConstants.GET)
+	@GetMapping(ISTOREConstants.GET)
 	public ResponseEntity<Product> get(@RequestParam Integer id) {
 		return new ResponseEntity<Product>(productService.get(id), HttpStatus.OK);
 	}
 
-	@PostMapping(ISTOREConstants.GET_ALL)
+	@GetMapping(ISTOREConstants.GET_ALL)
 	public ResponseEntity<List<Product>> getAll() {
 		return new ResponseEntity<List<Product>>(productService.getAll(), HttpStatus.OK);
 	}
