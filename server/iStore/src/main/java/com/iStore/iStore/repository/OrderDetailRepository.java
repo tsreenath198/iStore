b67b3d7865287d932bc5b454608d9b99473fc559
pavankumar2107 <36192034+pavankumar2107@users.noreply.github.com>
2020-03-10 12:00:01 +0530

    changed condition

--- server/iStore/src/main/java/com/iStore/iStore/repository/OrderDetailRepository.java
+++ server/iStore/src/main/java/com/iStore/iStore/repository/OrderDetailRepository.java
@@ -25,7 +25,7 @@ public interface OrderDetailRepository extends CrudRepository<OrderDetail, Integ
 	@Query(value = "SELECT DISTINCT * FROM order_detail WHERE DATE(created_date)= :dt and active_flag=0", nativeQuery = true)
 	List<OrderDetail> findAllByCreatedDate(String dt);
 
-	@Query(value = "SELECT * FROM order_detail WHERE created_date >= :fromDate and created_date <=:toDate and active_flag=0 ORDER BY created_date DESC", nativeQuery = true)
+	@Query(value = "SELECT * FROM order_detail WHERE created_date >= :fromDate and created_date <:toDate and active_flag=0 ORDER BY created_date DESC", nativeQuery = true)
 	List<OrderDetail> findAllBetweenDates(@Param(value = "fromDate") Date fromDate,
 			@Param(value = "toDate") Date toDate);
 
@@ -37,13 +37,13 @@ public interface OrderDetailRepository extends CrudRepository<OrderDetail, Integ
 
 	@Query(value = "SELECT o.id as id,o.total as total,o.created_date as createdDate,o.updated_date as updatedDate,o.description as description,"
 			+ "o.total_discount as totalDiscount,o.active_flag as activeFlag,o.payment_mode as paymentMode,o.contact as contact, MONTH(o.created_date) as month "
-			+ "FROM order_detail o WHERE o.created_date >= :fromDate and o.created_date <=:toDate and o.active_flag=0 ORDER BY o.created_date  DESC ", nativeQuery = true)
+			+ "FROM order_detail o WHERE o.created_date >= :fromDate and o.created_date <:toDate and o.active_flag=0 ORDER BY o.created_date  DESC ", nativeQuery = true)
 	List<OrderDetails> getSalesByType(@Param(value = "fromDate") String fromDate,
 			@Param(value = "toDate") String toDate);
 
 	@Query(value = "select od.id as id,year(od.created_date) as year,month(od.created_date) as month,'year' as type, year(od.created_date) as value, "
 			+ " ROUND(SUM(CASE WHEN od.payment_mode='Cash' THEN od.total ELSE 0 END)) as cashSum ,ROUND(SUM(CASE WHEN od.payment_mode='Bank' "
-			+ "THEN od.total ELSE 0 END)) as bankSum , ROUND(SUM(od.total)) as sum from order_detail od where od.created_date > :fromDate and od.created_date <= :toDate"
+			+ "THEN od.total ELSE 0 END)) as bankSum , ROUND(SUM(od.total)) as sum from order_detail od where od.created_date > :fromDate and od.created_date <:toDate"
 			+ " and od.active_flag=0 group by Year(od.created_date) ORDER BY od.created_date DESC", nativeQuery = true)
 	List<OrderDetailGroupInterface> findAllByGroupYears(@Param(value = "fromDate") String fromDate,
 			@Param(value = "toDate") String toDate);
@@ -51,21 +51,21 @@ public interface OrderDetailRepository extends CrudRepository<OrderDetail, Integ
 	@Query(value = "select od.id as id,year(od.created_date) as year,month(od.created_date) as month,'month' as type, Month(od.created_date) as value,"
 			+ " ROUND(SUM(CASE WHEN od.payment_mode='Cash' THEN od.total ELSE 0 END)) as cashSum ,ROUND(SUM(CASE WHEN od.payment_mode='Bank' "
 			+ "THEN od.total ELSE 0 END)) as bankSum , ROUND(SUM(od.total)) as sum from order_detail od where od.created_date > :fromDate and "
-			+ "od.created_date <= :toDate and od.active_flag=0 group by Month(od.created_date) ORDER BY od.created_date DESC", nativeQuery = true)
+			+ "od.created_date <:toDate and od.active_flag=0 group by Month(od.created_date) ORDER BY od.created_date DESC", nativeQuery = true)
 	List<OrderDetailGroupInterface> findAllByGroupMonths(@Param(value = "fromDate") String fromDate,
 			@Param(value = "toDate") String toDate);
 
 	@Query(value = "select od.id as id,year(od.created_date) as year,month(od.created_date) as month,'day' as type, Day(od.created_date) as value,"
 			+ " ROUND(SUM(CASE WHEN od.payment_mode='Cash' THEN od.total ELSE 0 END)) as cashSum ,ROUND(SUM(CASE WHEN od.payment_mode='Bank' "
 			+ "THEN od.total ELSE 0 END)) as bankSum , ROUND(SUM(od.total)) as sum from order_detail od where od.created_date > :fromDate and "
-			+ "od.created_date <= :toDate and od.active_flag=0 group by Year(od.created_date), Month(od.created_date) , Day(od.created_date) ORDER BY created_date DESC", nativeQuery = true)
+			+ "od.created_date <:toDate and od.active_flag=0 group by Year(od.created_date), Month(od.created_date) , Day(od.created_date) ORDER BY created_date DESC", nativeQuery = true)
 	List<OrderDetailGroupInterface> findAllByGroupDays(@Param(value = "fromDate") String fromDate,
 			@Param(value = "toDate") String toDate);
 
 	@Query(value = "select od.id as id,year(od.created_date) as year,month(od.created_date) as month,'month' as type, Month(od.created_date) as value, "
 			+ " ROUND(SUM(CASE WHEN od.payment_mode='Cash' THEN od.total ELSE 0 END)) as cashSum ,ROUND(SUM(CASE WHEN od.payment_mode='Bank' "
 			+ "THEN od.total ELSE 0 END)) as bankSum , ROUND(SUM(od.total)) as sum  from order_detail od where year(od.created_date)=:value "
-			+ "and  od.created_date > :fromDate and od.created_date <= :toDate and od.active_flag=0"
+			+ "and  od.created_date > :fromDate and od.created_date <:toDate and od.active_flag=0"
 			+ " group by Month(od.created_date) ORDER BY created_date DESC", nativeQuery = true)
 	List<OrderDetailGroupInterface> findAllByYear(@Param(value = "value") Integer value,
 			@Param(value = "fromDate") String fromDate, @Param(value = "toDate") String toDate);
@@ -73,7 +73,7 @@ public interface OrderDetailRepository extends CrudRepository<OrderDetail, Integ
 	@Query(value = "select od.id as id,year(od.created_date) as year,month(od.created_date) as month,'day' as type, Day(od.created_date) as value, "
 			+ " ROUND(SUM(CASE WHEN od.payment_mode='Cash' THEN od.total ELSE 0 END)) as cashSum ,ROUND(SUM(CASE WHEN od.payment_mode='Bank' "
 			+ "THEN od.total ELSE 0 END)) as bankSum , ROUND(SUM(od.total)) as sum from order_detail od where month(od.created_date)=:value and year(od.created_date)=:year "
-			+ "and  od.created_date > :fromDate and od.created_date <= :toDate and od.active_flag=0"
+			+ "and  od.created_date > :fromDate and od.created_date <:toDate and od.active_flag=0"
 			+ " group by Day(od.created_date) ORDER BY created_date DESC", nativeQuery = true)
 	List<OrderDetailGroupInterface> findAllByMonth(@Param(value = "year") Integer year,
 			@Param(value = "value") Integer value, @Param(value = "fromDate") String fromDate,
@@ -82,7 +82,7 @@ public interface OrderDetailRepository extends CrudRepository<OrderDetail, Integ
 	@Query(value = "select od.id as id,year(od.created_date) as year,month(od.created_date) as month,'day' as type, Day(od.created_date) as value, "
 			+ " ROUND(SUM(CASE WHEN od.payment_mode='Cash' THEN od.total ELSE 0 END)) as cashSum ,ROUND(SUM(CASE WHEN od.payment_mode='Bank' "
 			+ "THEN od.total ELSE 0 END)) as bankSum , ROUND(SUM(od.total)) as sum  from order_detail od where month(od.created_date)=:month and year(od.created_date)=:year"
-			+ " and day(od.created_date)=:value and  od.created_date > :fromDate and od.created_date <= :toDate and od.active_flag=0 "
+			+ " and day(od.created_date)=:value and  od.created_date > :fromDate and od.created_date <:toDate and od.active_flag=0 "
 			+ "group by Day(od.created_date) ORDER BY created_date DESC", nativeQuery = true)
 	List<OrderDetailGroupInterface> findAllByDay(@Param(value = "year") Integer year,
 			@Param(value = "month") Integer month, @Param(value = "value") Integer value,
