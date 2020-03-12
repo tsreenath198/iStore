4966de7bedb1e1a3891469a971953069338d78a2
pavankumar2107 <36192034+pavankumar2107@users.noreply.github.com>
2020-03-12 18:45:02 +0530

    sales by product quantity

--- server/iStore/src/main/java/com/iStore/iStore/repository/OrderDetailRepository.java
+++ server/iStore/src/main/java/com/iStore/iStore/repository/OrderDetailRepository.java
@@ -93,13 +93,17 @@ public interface OrderDetailRepository extends CrudRepository<OrderDetail, Integ
 	List<OrderDetail> findAllRecordsByDay(@Param(value = "year") Integer year, @Param(value = "month") Integer month,
 			@Param(value = "value") Integer value);
 
-	@Query(value = "SELECT DISTINCT c.name as name,	Count(c.name) as count FROM  product p,category c,item i,order_detail od where p.category_id=c.id and i.product_id=p.id"
-			+ " and i.order_id=od.id and  od.created_date > :fromDate and od.created_date <:toDate and od.active_flag=0 GROUP by c.name  ", nativeQuery = true)
+	@Query(value = "SELECT c.name as name,	SUM(i.quantity) as count FROM "
+			+ "order_detail od left outer join item i on od.id = i.order_id "
+			+ "left outer join product p on i.product_id = p.id left outer join category c on p.category_id = c.id "
+			+ "where od.created_date > :fromDate and od.created_date <:toDate and od.active_flag=0 GROUP by c.name", nativeQuery = true)
 	List<CategoryDetailInterface> getSalesByCategory(@Param(value = "fromDate") String fromDate,
 			@Param(value = "toDate") String toDate);
 
-	@Query(value = "SELECT  p.name as name,	Count(p.name) as count FROM  product p,category c,item i,order_detail od where p.category_id=c.id and i.product_id=p.id"
-			+ " and i.order_id=od.id and  od.created_date > :fromDate and od.created_date <:toDate and od.active_flag=0 and c.name=:category GROUP by p.name  ", nativeQuery = true)
+	@Query(value = "SELECT p.name as name,	SUM(i.quantity) as count FROM "
+			+ "order_detail od left outer join item i on od.id = i.order_id "
+			+ "left outer join product p on i.product_id = p.id left outer join category c on p.category_id = c.id "
+			+ "where od.created_date > :fromDate and od.created_date <:toDate and c.name=:category and od.active_flag=0 GROUP by p.name", nativeQuery = true)
 	List<CategoryDetailInterface> getProductByProduct(@Param(value = "category") String category,
 			@Param(value = "fromDate") String fromDate, @Param(value = "toDate") String toDate);
 
