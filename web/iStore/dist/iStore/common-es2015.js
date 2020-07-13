74edb943a0334edbafc7aea41455850a8a4f57f3
gangasaiuskcorp <gangasai.uskcorp@gmail.com>
2020-07-13 15:30:05 +0530

    dist folder updated

---
+++ web/iStore/dist/iStore/common-es2015.js
@@ -0,0 +1,160 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["common"],{
+
+/***/ "./src/app/constants/url-constants.ts":
+/*!********************************************!*\
+  !*** ./src/app/constants/url-constants.ts ***!
+  \********************************************/
+/*! exports provided: URLConstants */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "URLConstants", function() { return URLConstants; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+
+class URLConstants {
+    constructor() {
+        /*product*/
+        this.ProductCreate = 'product/create';
+        this.ProductGetAll = 'product/getAll';
+        this.ProductUpdate = 'product/update';
+        this.ProductDelete = 'product/delete?id=';
+        this.ProductGetById = 'product/get?id=';
+        this.ProductGetInventory = 'product/getCurrentInventory';
+        this.ProductInventoryUpdate = 'product/setInventory';
+        this.GetRawInventory = 'product/downloadInventory';
+        /**Order */
+        this.OrderCreate = 'order/create';
+        this.OrderGetAll = 'order/getAll';
+        this.OrderUpdate = 'order/update';
+        this.OrderDelete = 'order/delete?id=';
+        this.OrderGetById = 'order/get?id=';
+        this.OrderTotalByDays = 'order/getTotalByDays?days=';
+        this.OrderGetId = 'order/getTotalRecordCount';
+        /**Reporr */
+        this.ReportByGroup = 'report/getReportTotalByGroup?fromDate=';
+        this.ReportByValue = 'report/getReportTotalByValue?value=';
+        this.ReportGetBills = 'report/getAllRecordsByDay?month=';
+        /**Category */
+        this.CategoryCreate = 'category/create';
+        this.CategoryGetAll = 'category/getAll';
+        this.CategoryUpdate = 'category/update';
+        this.CategoryDelete = 'category/delete?id=';
+        this.CategoryGetById = 'category/get?id=';
+        /** User */
+        this.UserCreate = 'user/create';
+        this.UserGetAll = 'user/getAll';
+        this.UserUpdate = 'user/update';
+        this.UserDelete = 'user/delete?id=';
+        this.UserGetById = 'user/get?id=';
+        /** Expense */
+        this.ExpenseCreate = 'expense/create';
+        this.ExpenseGetAll = 'expense/getAll';
+        this.ExpenseUpdate = 'expense/update';
+        this.ExpenseDelete = 'expense/delete?id=';
+        this.ExpenseGetById = 'expense/get?id=';
+        /**Customers */
+        this.CustomerCreate = 'contact/create';
+        this.CustomerGetAll = 'contact/getAll';
+        this.CustomerUpdate = 'contact/update';
+        this.CustomerDelete = 'contact/delete?id=';
+        this.CustomerGetById = 'contact/get?id=';
+        /** Login */
+        this.Login = 'login/validateUser?name=';
+        /**Sales */
+        this.SalesGetCategories = 'sales/getByCategory?fromDate=';
+        this.SalesGetProducts = 'sales/getByProduct?category=';
+    }
+}
+
+
+/***/ }),
+
+/***/ "./src/app/services/http.service.ts":
+/*!******************************************!*\
+  !*** ./src/app/services/http.service.ts ***!
+  \******************************************/
+/*! exports provided: HttpService */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "HttpService", function() { return HttpService; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm2015/core.js");
+/* harmony import */ var _angular_common_http__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/common/http */ "./node_modules/@angular/common/fesm2015/http.js");
+
+
+
+let HttpService = class HttpService {
+    constructor(http) {
+        this.http = http;
+        this.base_url = "http://localhost:8081/";
+    }
+    post(data, url) {
+        return this.http.post(this.base_url + url, data);
+    }
+    put(data, url) {
+        return this.http.put(this.base_url + url, data);
+    }
+    postImage(url, data) {
+        return this.http.post(this.base_url + url, data);
+    }
+    get(URL) {
+        return this.http.get(this.base_url + URL);
+    }
+    update(data, URL) {
+        return this.http.put(this.base_url + URL, data);
+    }
+    delete(URL) {
+        return this.http.delete(this.base_url + URL);
+    }
+};
+HttpService.ctorParameters = () => [
+    { type: _angular_common_http__WEBPACK_IMPORTED_MODULE_2__["HttpClient"] }
+];
+HttpService = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+    Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["Injectable"])({
+        providedIn: 'root'
+    })
+], HttpService);
+
+
+
+/***/ }),
+
+/***/ "./src/app/services/storage.service.ts":
+/*!*********************************************!*\
+  !*** ./src/app/services/storage.service.ts ***!
+  \*********************************************/
+/*! exports provided: StorageService */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "StorageService", function() { return StorageService; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm2015/core.js");
+
+
+let StorageService = class StorageService {
+    constructor() { }
+    getLoggedInUserName() {
+        if (localStorage.getItem("loggedInUser") != "null") {
+            this.user = JSON.parse(localStorage.getItem("loggedInUser"));
+            return this.user;
+        }
+    }
+};
+StorageService = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+    Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["Injectable"])({
+        providedIn: "root"
+    })
+], StorageService);
+
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=common-es2015.js.map
