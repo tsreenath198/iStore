3e46d91cfc70d0739b3e54336fa301db36ec3d07
gangasaiuskcorp <gangasai.uskcorp@gmail.com>
2021-01-15 17:37:22 +0530

    Fixed edit popup issues

--- web/iStore/src/app/components/layout/report/edit-report/edit-report.component.ts
+++ web/iStore/src/app/components/layout/report/edit-report/edit-report.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { URLConstants } from 'src/app/constants/url-constants';
+import { HttpService } from '../../../../services/http.service';
 
 @Component({
   selector: 'app-edit-report',
@@ -7,14 +9,19 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class EditReportComponent implements OnInit {
   @Input() report:any;
+  public url = new URLConstants();
   public paymentTypes: Array<any> = ["Cash", "Bank"];
   public orderTypes: Array<any> = ["Store", "Zomato", "Swiggy"];
-  constructor() { }
+  constructor(private http:HttpService) { }
 
   ngOnInit() {
+    this.report.invoiceDate = new Date(this.report.invoiceDate).toISOString().substring(0, 10);
   }
   public update() {
-    
+    this.http.update(this.report, this.url.OrderUpdate).subscribe(resp => {
+      window.alert("Updated successfully.");
+      window.location.reload();
+    });
   }
 
 }
